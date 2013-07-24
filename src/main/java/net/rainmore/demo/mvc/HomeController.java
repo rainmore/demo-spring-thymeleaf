5478aaf4a0c27c47da0b2904ff3aab71c742e098
Jie Rong (Felix) <rainmore24@gmail.com>
2013-07-25 07:24:02 +1000

    locale works

--- src/main/java/net/rainmore/demo/mvc/HomeController.java
+++ src/main/java/net/rainmore/demo/mvc/HomeController.java
@@ -1,18 +1,25 @@
 package net.rainmore.demo.mvc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController extends AbstractController {
 
+    @Autowired
+    private org.springframework.context.MessageSource messageSource;
+
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hello world!");
+        model.addAttribute("hello", "Hello world!");
         return "pages/home/default";
     }
 
